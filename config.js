5377fc980cba870a43f492984204c6cc988f7198
erwanwahyudi07 <erwanwahyudi07@gmail.com>
2026-08-04 07:52:26 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // (didapat setelah deploy Code.gs sebagai Web App - lihat panduan terpisah).
 // Contoh: "https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec"
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycby-zg-JhR4pnu8YONL3Mfq4t093EXwI5QTl90EGExrszdq5l8cwhaSZC-Ba5tf2HrKk/exec",
+  API_URL: "PASTE_URL_APPS_SCRIPT_ANDA_DI_SINI",
   NAMA_KOMPLEK: "Vertihouse Sawangan II"
 };
