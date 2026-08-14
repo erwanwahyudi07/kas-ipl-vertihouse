3c02847b47dfaf5cdb41424df45701d25c941cd4
erwanwahyudi07 <erwanwahyudi07@gmail.com>
2026-08-14 22:38:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
-
 // GANTI nilai di bawah ini dengan URL Web App Apps Script Anda
 // (didapat setelah deploy Code.gs sebagai Web App - lihat panduan terpisah).
 // Contoh: "https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec"
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycby8XdwgpG0R_yroF_8O_u1ofqrIE6kRnWYgkQ8K8_TCcTRt3Hg9ATuVpFAKADt0nVVhYg/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbwEn9YeqWJiA7NStm01D3ubbzdzmM9Nw1wktoAmUu5Z0Pgxq-SjW3LSwVZSwI9-6RxS/exec",
   NAMA_KOMPLEK: "Vertihouse Sawangan II"
 };
